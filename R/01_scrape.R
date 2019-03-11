2babed6961b13aa503ef704c25533fbcd79a2644
Jonathan Spring <jon.spring@gmail.com>
2019-03-10 23:15:44 -0700

    basic table download works for single log, with some bugs

--- R/01_scrape.R
+++ R/01_scrape.R
@@ -10,3 +10,18 @@ log_pages <- scrape(session)  %>% #, params="t=semi-soft&per_page=100") %>%
   map(~html_nodes(., "a")) %>%
   pluck(1) %>%
   xml_attr("href")
+
+# Now that we know the available pages, scrape.
+
+
+
+# Test with one page
+session2 <- bow(log_pages[1], force = TRUE)
+
+log_raw <- scrape(session2) %>%
+  html_nodes(".post-content") %>%
+  pluck(1) %>%
+  html_node("table") %>%
+  html_table(fill = TRUE, header = T)
+
+
